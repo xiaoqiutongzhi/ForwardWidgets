ff2bf361d3019946db64985f48fad0748a7c9ca6
huangxd- <649924757@qq.com>
2025-05-17 07:13:23 +0800

    重定向获取真实直播链接

--- widgets/live-tv.js
+++ widgets/live-tv.js
@@ -29,7 +29,7 @@ WidgetMetadata = {
             ],
         },
     ],
-    version: "1.0.10",
+    version: "1.0.11",
     requiredVersion: "0.0.1",
     description: "解析电视直播订阅链接【五折码：CHEAP.5;七折码：CHEAP】",
     author: "huangxd",
@@ -192,7 +192,7 @@ async function loadDetail(link) {
 
     let videoUrl = link;
 
-    await sendMSG(JSON.stringify(response.body));
+    await sendMSG(JSON.stringify(response.data));
     await sendMSG(JSON.stringify(response.headers));
 
     // 检查是否是302并且有Location头
