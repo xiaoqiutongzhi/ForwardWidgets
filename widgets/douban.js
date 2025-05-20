bda603dab5ba81feb563662eeb83d299b8b96ef2
huangxd- <649924757@qq.com>
2025-05-20 22:55:04 +0800

    新增豆瓣片单(TMDB版)

--- widgets/douban.js
+++ widgets/douban.js
@@ -73,8 +73,78 @@ WidgetMetadata = {
         },
       ],
     },
+    {
+      title: "豆瓣片单(TMDB版)",
+      requiresWebView: false,
+      functionName: "loadCardItems",
+      params: [
+        {
+          name: "url",
+          title: "列表地址",
+          type: "input",
+          description: "豆瓣片单地址",
+          placeholders: [
+            {
+              title: "影院热映",
+              value: "https://m.douban.com/subject_collection/movie_showing",
+            },
+            {
+              title: "实时热门电影",
+              value: "https://m.douban.com/subject_collection/movie_real_time_hotest",
+            },
+            {
+              title: "实时热门电视",
+              value: "https://m.douban.com/subject_collection/tv_real_time_hotest",
+            },
+            {
+              title: "实时热门综艺",
+              value: "https://m.douban.com/subject_collection/show_hot",
+            },
+            {
+              title: "豆瓣 Top 250",
+              value: "https://m.douban.com/subject_collection/movie_top250",
+            },
+            {
+              title: "一周电影口碑榜",
+              value: "https://m.douban.com/subject_collection/movie_weekly_best",
+            },
+            {
+              title: "华语口碑剧集榜",
+              value: "https://m.douban.com/subject_collection/tv_chinese_best_weekly",
+            },
+            {
+              title: "全球口碑剧集榜",
+              value: "https://m.douban.com/subject_collection/tv_global_best_weekly",
+            },
+            {
+              title: "国内综艺口碑榜",
+              value: "https://m.douban.com/subject_collection/show_chinese_best_weekly",
+            },
+            {
+              title: "全球综艺口碑榜",
+              value: "https://m.douban.com/subject_collection/show_global_best_weekly",
+            },
+            {
+              title: "第97届奥斯卡",
+              value: "https://m.douban.com/subject_collection/EC7I7ZDRA?type=rank",
+            },
+          ],
+        },
+        {
+          name: "start",
+          title: "开始",
+          type: "count",
+        },
+        {
+          name: "limit",
+          title: "每页数量",
+          type: "constant",
+          value: "20",
+        },
+      ],
+    },
   ],
-  version: "1.0.2",
+  version: "1.0.3",
   requiredVersion: "0.0.1",
   description: "解析豆瓣想看、在看、已看以及根据个人数据生成的个性化推荐【五折码：CHEAP.5;七折码：CHEAP】",
   author: "huangxd",
@@ -136,4 +206,106 @@ async function loadSuggestionItems(params = {}) {
     return doubanIds;
   }
   return [];
+}
+
+// 基础获取TMDB数据方法
+async function fetchTmdbData(key, mediaType) {
+    const tmdbResults = await Widget.tmdb.get(`/search/${mediaType}`, {
+        params: {
+            query: key,
+            language: "zh_CN",
+        }
+    });
+    //打印结果
+    // console.log("搜索内容：" + key)
+    // console.log("tmdbResults:" + JSON.stringify(tmdbResults, null, 2));
+    // console.log("tmdbResults.total_results:" + tmdbResults.total_results);
+    // console.log("tmdbResults.results[0]:" + tmdbResults.results[0]);
+    return tmdbResults.results;
+}
+
+// 解析豆瓣片单
+async function loadCardItems(params = {}) {
+  try {
+    console.log("开始解析豆瓣片单...");
+    console.log("参数:", params);
+    // 获取片单 URL
+    const url = params.url;
+    if (!url) {
+      console.error("缺少片单 URL");
+      throw new Error("缺少片单 URL");
+    }
+    // 验证 URL 格式
+    if (url.includes("douban.com/subject_collection/")) {
+      return loadSubjectCollection(params);
+    }
+  } catch (error) {
+    console.error("解析豆瓣片单失败:", error);
+    throw error;
+  }
+}
+
+async function loadItemsFromApi(params = {}) {
+  const url = params.url;
+  console.log("请求 API 页面:", url);
+  const listId = params.url.match(/subject_collection\/(\w+)/)?.[1];
+  const response = await Widget.http.get(url, {
+    headers: {
+      Referer: `https://m.douban.com/subject_collection/${listId}/`,
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    },
+  });
+
+  console.log("请求结果:", response.data);
+  if (response.data && response.data.subject_collection_items) {
+    const scItems = response.data.subject_collection_items;
+
+    const promises = scItems.map(async (scItem) => {
+        // 模拟API请求
+        const tmdbDatas = await fetchTmdbData(scItem.title, scItem.type)
+
+        if (tmdbDatas.length !== 0) {
+            return {
+                id: tmdbDatas[0].id,
+                type: "tmdb",
+                title: tmdbDatas[0].title ?? tmdbDatas[0].name,
+                description: tmdbDatas[0].overview,
+                releaseDate: tmdbDatas[0].release_date ?? tmdbDatas[0].first_air_date,
+                backdropPath: tmdbDatas[0].backdrop_path,
+                posterPath: tmdbDatas[0].poster_path,
+                rating: tmdbDatas[0].vote_average,
+                mediaType: mediaTypeDict[genre],
+            };
+        } else {
+            return null;
+        }
+    });
+
+    // 等待所有请求完成
+    const items = (await Promise.all(promises)).filter(Boolean);
+
+    console.log(items)
+
+    return items;
+  }
+  return [];
+}
+
+async function loadSubjectCollection(params = {}) {
+  const listId = params.url.match(/subject_collection\/(\w+)/)?.[1];
+  console.debug("合集 ID:", listId);
+  if (!listId) {
+    console.error("无法获取合集 ID");
+    throw new Error("无法获取合集 ID");
+  }
+
+  const start = params.start || 0;
+  const limit = params.limit || 20;
+  let pageUrl = `https://m.douban.com/rexxar/api/v2/subject_collection/${listId}/items?start=${start}&count=${limit}&updated_at&items_only=1&type_tag&for_mobile=1`;
+  if (params.type) {
+    pageUrl += `&type=${params.type}`;
+  }
+  params.url = pageUrl;
+  return await loadItemsFromApi(params);
 }
