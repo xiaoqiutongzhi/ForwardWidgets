d4f2d967230745dac41d2d161fe4ba45dde14933
huangxd- <649924757@qq.com>
2025-05-29 22:57:16 +0800

    雅图插件点播排行榜和评分排行榜增加右上角排序组件适配

--- widgets/yatu.js
+++ widgets/yatu.js
@@ -123,7 +123,7 @@ WidgetMetadata = {
                     ],
                 },
                 {
-                    name: "time",
+                    name: "order_by",
                     title: "时间",
                     type: "enumeration",
                     enumOptions: [
@@ -168,7 +168,7 @@ WidgetMetadata = {
                     ],
                 },
                 {
-                    name: "level",
+                    name: "order_by",
                     title: "等级",
                     type: "enumeration",
                     enumOptions: [
@@ -193,7 +193,7 @@ WidgetMetadata = {
             ],
         },
     ],
-    version: "1.0.3",
+    version: "1.0.4",
     requiredVersion: "0.0.1",
     description: "解析雅图每日放送更新以及各类排行榜【五折码：CHEAP.5;七折码：CHEAP】",
     author: "huangxd",
@@ -445,7 +445,7 @@ async function fetchFinalItems(genre, typ, mediaTypeDict, suffixDict) {
 async function loadClickItems(params = {}) {
     try {
         const genre = params.genre || "";
-        const typ = params.time || "";
+        const typ = params.order_by || "";
 
         if (!genre || !typ) {
             throw new Error("必须提供分类、时间");
@@ -483,7 +483,7 @@ async function loadClickItems(params = {}) {
 async function loadScoreItems(params = {}) {
     try {
         const genre = params.genre || "";
-        const typ = params.level || "";
+        const typ = params.order_by || "";
 
         if (!genre || !typ) {
             throw new Error("必须提供分类、等级");
