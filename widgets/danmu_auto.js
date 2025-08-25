86a1025d90a8d4d6709c8f9df62d8726eb35efd4
huangxd- <649924757@qq.com>
2025-08-25 21:41:39 +0800

    自动链接弹幕完善没有找到播放链接的场景下使用弹幕API的逻辑

--- widgets/danmu_auto.js
+++ widgets/danmu_auto.js
@@ -15,7 +15,7 @@
 WidgetMetadata = {
   id: "forward.auto.danmu",
   title: "自动链接弹幕",
-  version: "1.0.14",
+  version: "1.0.15",
   requiredVersion: "0.0.2",
   description: "自动获取播放链接并从服务器获取弹幕【五折码：CHEAP.5;七折码：CHEAP】",
   author: "huangxd",
@@ -1688,6 +1688,13 @@ async function getCommentsById(params) {
             if (playUrl) {
                 return await getDanmuFromUrl(danmu_server, playUrl, debug, danmu_server_polling);
             }
+            if (!playUrl && api_priority === "false") {
+                const result = await getDanmuFromAPI(title, tmdbInfo, type, season, episode, episodeName, airDate,
+                    danmu_api_1, danmu_api_2, danmu_api_3, danmu_api_4, danmu_api_5);
+                if (result) {
+                  return result;
+                }
+            }
             if (!playUrl) {
                 const count = debug === "true" ? 24 : 1;
                 return generateDanmaku("【自动链接弹幕】：没有找到该集弹幕", count);
@@ -1713,6 +1720,13 @@ async function getCommentsById(params) {
           playUrl = await getPlayurlFromVod(title, tmdbInfo, type, season, episode, episodeName, airDate, platform, vod_site, vod_site_polling);
           if (playUrl) {
               return await getDanmuFromUrl(danmu_server, playUrl, debug, danmu_server_polling);
+          }
+          if (!playUrl && api_priority === "false") {
+              const result = await getDanmuFromAPI(title, tmdbInfo, type, season, episode, episodeName, airDate,
+                  danmu_api_1, danmu_api_2, danmu_api_3, danmu_api_4, danmu_api_5);
+              if (result) {
+                return result;
+              }
           }
           if (!playUrl) {
               const count = debug === "true" ? 24 : 1;
@@ -1791,6 +1805,13 @@ async function getCommentsById(params) {
       playUrl = await getPlayurlFromVod(title, tmdbInfo, type, season, episode, episodeName, airDate, platform, vod_site, vod_site_polling);
       if (playUrl) {
           return await getDanmuFromUrl(danmu_server, playUrl, debug, danmu_server_polling);
+      }
+        if (!playUrl && api_priority === "false") {
+          const result = await getDanmuFromAPI(title, tmdbInfo, type, season, episode, episodeName, airDate,
+              danmu_api_1, danmu_api_2, danmu_api_3, danmu_api_4, danmu_api_5);
+          if (result) {
+            return result;
+          }
       }
       if (!playUrl) {
           const count = debug === "true" ? 24 : 1;
